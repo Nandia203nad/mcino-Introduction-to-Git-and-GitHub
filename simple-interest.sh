fa9c9443102937fa5ad207dcd818ee6f0805a21b
Nandia203nad <nadyanandia203@gmail.com>
2026-05-27 20:12:02 +0800

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,46 +1,25 @@
 #!/bin/bash
-# This script calculates simple interest given principal, annual rate of interest and time period in years.
-# Do not use this in production. Sample purpose only.
+# Энэхүү скрипт нь хэрэглэгчийн оруулсан өгөгдөл дээр үндэслэн энгийн хүүг бодно.
+# Ашиглах томьёо: SI = (Principal * Rate * Time) / 100
 
-# Author: Upkar Lidder (IBM)
-# Addtional Authors:
-# <your Github username>
+echo "--------------------------------------------------"
+echo "   Энгийн хүү бодогч програм (Simple Interest)    "
+echo "--------------------------------------------------"
 
-# Input:
-# p, principal amount
-# t, time period in years
-# r, annual rate of interest
+# 1. Хэрэглэгчээс оролтын талбаруудыг асууж авах
+read -p "Үндсэн хөрөнгө (Principal): " principal
+read -p "Жилийн хүүгийн хувь % (Rate of Interest): " rate
+read -p "Хугацаа /жилээр/ (Time Period): " time
 
-# Output:
-# simple interest = p*t*r
+# 2. Энгийн хүүг 'bc' ашиглан бутархай орны нарийвчлалтай бодох
+# 'scale=2' нь таслалаас хойш 2 орон харуулна гэдгийг зааж байна.
+simple_interest=$(echo "scale=2; ($principal * $rate * $time) / 100" | bc)
 
-echo "Enter the principal:"
-read p
-echo "Enter rate of interest per year:"
-read r
-echo "Enter time period in years:"
-read t
+# 3. Нийт үнийн дүнг бодох (Үндсэн хөрөнгө + Хүү)
+total_amount=$(echo "scale=2; $principal + $simple_interest" | bc)
 
-s=$(expr $p \* $t \* $r / 100)
-echo "The simple interest is: "
-echo $s
-#!/bin/bash
-# Энэхүү ск립т нь энгийн хүүг бодож гаргана.
-# Зохиогч: Таны нэр
-# Формула: SI = (P * R * T) / 100
-
-echo "--- Энгийн хүү бодогч програм ---"
-
-# Хэрэглэгчээс утгуудыг авах
-read -p "Үндсэн хөрөнгө (Principal): " p
-read -p "Жилийн хүүгийн хувь (Rate of interest %): " r
-read -p "Хугацаа /жилээр/ (Time period in years): " t
-
-# Энгийн хүүг бодох (SI)
-# Bash нь бутархай тоо шууд боддоггүй тул 'bc' ашиглав
-interest=$(echo "scale=2; ($p * $r * $t) / 100" | bc)
-total=$(echo "scale=2; $p + $interest" | bc)
-
-echo "---------------------------------"
-echo "Бодогдсон хүү (Simple Interest): $interest"
-echo "Нийт дүн (Эх үүсвэр + Хүү): $total"
+# 4. Үр дүнг дэлгэцэнд тодорхой харуулах
+echo "--------------------------------------------------"
+echo "Бодогдсон энгийн хүү: $simple_interest"
+echo "Нийт эргэн төлөгдөх дүн: $total_amount"
+echo "--------------------------------------------------"
