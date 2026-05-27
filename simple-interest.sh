d0bd6e56cc95e01e434c970cf95f1e47356f90b5
Nandia203nad <nadyanandia203@gmail.com>
2026-05-27 19:54:47 +0800

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -24,3 +24,23 @@ read t
 s=$(expr $p \* $t \* $r / 100)
 echo "The simple interest is: "
 echo $s
+#!/bin/bash
+# Энэхүү ск립т нь энгийн хүүг бодож гаргана.
+# Зохиогч: Таны нэр
+# Формула: SI = (P * R * T) / 100
+
+echo "--- Энгийн хүү бодогч програм ---"
+
+# Хэрэглэгчээс утгуудыг авах
+read -p "Үндсэн хөрөнгө (Principal): " p
+read -p "Жилийн хүүгийн хувь (Rate of interest %): " r
+read -p "Хугацаа /жилээр/ (Time period in years): " t
+
+# Энгийн хүүг бодох (SI)
+# Bash нь бутархай тоо шууд боддоггүй тул 'bc' ашиглав
+interest=$(echo "scale=2; ($p * $r * $t) / 100" | bc)
+total=$(echo "scale=2; $p + $interest" | bc)
+
+echo "---------------------------------"
+echo "Бодогдсон хүү (Simple Interest): $interest"
+echo "Нийт дүн (Эх үүсвэр + Хүү): $total"
